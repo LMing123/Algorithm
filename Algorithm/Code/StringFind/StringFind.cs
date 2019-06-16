5b4f029d7df7b2f744e6740408adb7f8c04d4f99
LMing123 <z971130192@live.com>
2019-06-16 19:03:28 +0800

    RabinKarp

---
+++ Algorithm/Code/StringFind/StringFind.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.StringFind
+{
+    public class StringFind
+    {
+        static readonly int MAXHASH = 26;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fuck">original</param>
+        /// <param name="shit">matched</param>
+        /// <returns></returns>
+        public static (bool, int) RabinKarp(string fuck, string shit)
+        {
+            int originalLength = fuck.Length;
+            int matchLength = shit.Length;
+            int shitHash = HashString(shit);
+            int fuckHash = HashString(fuck.Substring(0,matchLength));
+            for(int i=0;i<originalLength-matchLength+1;i++)
+            {
+                if(shitHash==fuckHash)
+                {
+                    int j = i, k = 0;
+                    for (;j<i+matchLength;j++,k++)
+                    {
+                        if(shit[k]!=fuck[j])
+                        {
+                            break;
+                        }
+                    }
+                    if (k == matchLength)
+                        return (true, i);
+                }
+                else
+                {
+                    if(i< originalLength - matchLength)
+                    {
+                        fuckHash = MAXHASH * (fuckHash - (int)Math.Pow(MAXHASH, matchLength - 1) * CharConvert(fuck[i])) + CharConvert(fuck[i + matchLength]);
+                    }
+                }
+            }
+            return (false, -1);
+
+        }
+
+        private static int HashString(string hstring)
+        {
+            int total = 0;
+            for(int i=0;i<hstring.Length;i++)
+            {
+                total += (int)Math.Pow(MAXHASH, hstring.Length - i - 1)*CharConvert(hstring[i]);
+            }
+            return total;
+        }
+        private static int CharConvert(char c) => (int)c - 'a';
+
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -4,16 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Algorithm.Code.LeetCode.simple;
+using Algorithm.Code.StringFind;
+
 namespace Algorithm
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            int[] nums1 ={-1,-1,2147483647    };
-            int[] nums2 = {1 };
-            Console.WriteLine(Solution_169.Fuck(nums1));
+            string c = "34196372";
+            string d = "1963";
+            string a = "abcabaabcabac";
+            string b = "abaa";
+            Console.WriteLine(StringFind.RabinKarp(a,b).Item1);
+            Console.WriteLine(StringFind.RabinKarp(a,b).Item2);
             //var items = Solution_100.CreateDiffTree();
             //var shit = Solution_100.Fuck(items.q, items.p);
            // Console.WriteLine(shit);
