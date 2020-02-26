597618cd30062872e0ea158d45ab82228e71758b
LMing123 <z971130192@live.com>
2020-02-26 19:04:16 +0800

    Update Solution_112.cs

--- Algorithm/Code/LeetCode/simple/Solution_112.cs
+++ Algorithm/Code/LeetCode/simple/Solution_112.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace Algorithm.Code.LeetCode.simple
 {
+    /// <summary>
+    /// 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+    /// 说明: 叶子节点是指没有子节点的节点。
+    /// [5,4,8,11,null,13,4,7,2,null,null,null,1],22  true
+    /// </summary>
     public class Solution_112
     {
         public static bool HasPathSum(TreeNode root, int sum)
