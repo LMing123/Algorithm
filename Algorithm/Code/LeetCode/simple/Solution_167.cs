715b7b70a2b04c6a4300198ce951966e90b56d29
LMing123 <z971130192@live.com>
2020-03-19 20:05:26 +0800

    = =
    头疼

---
+++ Algorithm/Code/LeetCode/simple/Solution_167.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    public class Solution_167
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            Dictionary<int, int> list = new Dictionary<int, int>();
+            for(int i=numbers.Length-1;i>=0;i--)
+            {
+                int value = target - numbers[i];
+                if(list.ContainsKey(value))
+                {
+                   return new int[] { i + 1, list[value]  };
+                }
+                if(list.ContainsKey(numbers[i]))
+                {
+                    continue;
+                }
+                list.Add(numbers[i], i + 1);
+            }
+            return new int[] { };
+        }
+    }
+}
