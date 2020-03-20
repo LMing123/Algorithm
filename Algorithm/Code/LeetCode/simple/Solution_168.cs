44ee0408e971eb55f596e1adc6064d7ad344582c
LMing123 <z971130192@live.com>
2020-03-20 20:28:46 +0800

    ....

---
+++ Algorithm/Code/LeetCode/simple/Solution_168.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    public class Solution_168
+    {
+        public static string ConvertToTitle(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            char a = 'A';
+
+            while (n > 0)
+            {
+                var i = n % 26;
+                if (i == 0)
+                {
+                    sb.Insert(0, 'Z');
+                    n = n - 26;
+                }
+                else
+                {
+                    sb.Insert(0, char.ConvertFromUtf32(a + i - 1));
+                }
+
+                n = n / 26;             
+
+            }
+            return sb.ToString();
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -33,7 +33,7 @@ namespace Algorithm
           //int[] vs = { 7, 1, 5, 3, 6, 4 };
             int[]  vs = { 3, 3, 5, 0, 0, 3, 1, 4 };
             // int[]  vs = { 1,2,3,4,5 };
-           
+           var i= Solution_168.ConvertToTitle(701);
         }
     }
 
