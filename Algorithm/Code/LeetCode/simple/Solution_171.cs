21e19ad90046d4ef3a77e3d231353e89ef605705
LMing123 <z971130192@live.com>
2020-03-23 19:37:25 +0800

    171

---
+++ Algorithm/Code/LeetCode/simple/Solution_171.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    public class Solution_171
+    {
+        public static int TitleToNumber(string s)
+        {
+            int length = s.Length;
+            char a = '@';
+            int result = 0;
+            int j = 0;
+            for(int i=length-1;i>=0;i--,j++)
+            {
+               result+= (int)Math.Pow(26, j)*(s[i]-a);
+
+            }
+            return result;
+        }
+    }
+}
