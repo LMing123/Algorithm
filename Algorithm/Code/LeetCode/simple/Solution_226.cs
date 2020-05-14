556c62858e53135310844232a8af210365b2c65c
LMing123 <z971130192@live.com>
2020-05-14 21:13:49 +0800

    add 226

---
+++ Algorithm/Code/LeetCode/simple/Solution_226.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    public class Solution_226
+    {
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+            InvertTree(root.left);
+            InvertTree(root.right);
+            var tem = root.left;
+            root.left = root.right;
+            root.right = tem;
+            return root;
+        }
+    }
+}
