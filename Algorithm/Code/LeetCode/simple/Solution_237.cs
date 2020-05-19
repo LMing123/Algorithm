67150458754ffa4fe846c3b829fc50057775224b
LMing123 <z971130192@live.com>
2020-05-19 19:51:34 +0800

    add 237  242

---
+++ Algorithm/Code/LeetCode/simple/Solution_237.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_237
+    {
+        public static void DeleteNode(ListNode node)
+        {
+            if (node == null) return;
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_242.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_242
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            HashSet<char> vs = new HashSet<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (vs.Contains(s[i])) continue;
+                vs.Add(s[i]);
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (!vs.Contains(t[i])) return false;
+            }
+
+            int result1 = 0;
+            int result2 = 0;
+            int length = s.Length > t.Length ? s.Length : t.Length;
+            for(int i=0;i<length;i++)
+            {
+                result1 += s.Length > i ? s[i] : 0;
+                result2 += t.Length > i ? t[i] : 0;
+            }
+            return result1 == result2 ? true:false;
+        }
+    }
+}
