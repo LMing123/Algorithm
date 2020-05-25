6c8867c3a0412b2ed0c4a8f5e9fb9b5c0466c026
LMing123 <z971130192@live.com>
2020-05-25 20:18:27 +0800

    add 263

---
+++ Algorithm/Code/LeetCode/simple/Solution_263.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_263
+    {
+        public bool IsUgly(int num)
+        {
+            if (num < 1) return false;
+            if (num == 1) return true;
+
+            while (num % 2 == 0) num = num / 2;
+            while (num % 3 == 0) num = num / 3;
+            while (num % 5 == 0) num = num / 5;
+
+            if (num == 1) return true;
+            return false;
+        }
+    }
+}
