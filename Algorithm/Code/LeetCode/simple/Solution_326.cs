5e66db69833e150162e9489e762403b840c36cff
LMing123 <z971130192@live.com>
2020-06-02 22:40:52 +0800

    add 326 342

---
+++ Algorithm/Code/LeetCode/simple/Solution_326.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_326
+    {
+        public static bool IsPowerOfThree(int n)
+        {
+            if (n <= 0) return false;
+            while(n!=0)
+            {
+                if (n % 3 != 0) return false;
+                n = n / 3;
+            }
+            return true;
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_342.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_342
+    {
+        public bool IsPowerOfFour(int num)
+        {
+            int i;
+            for (int j = 0; j <= 32; j++)
+            {
+                i = 1 << (j * 2);
+                if (i > num) break;
+                if (i == (i | num)) return true;
+            }
+            return false;
+        }
+    }
+}
