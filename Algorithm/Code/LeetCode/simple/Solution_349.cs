fb966f341ad2406d7e5d2771a7e7b42b1a6e58e8
LMing123 <z971130192@live.com>
2020-06-04 21:09:50 +0800

    add 349 350

---
+++ Algorithm/Code/LeetCode/simple/Solution_349.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_349
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            if (nums1.Length==0 || nums2.Length == 0) return new int[0];
+            var list = new HashSet<int>(nums1);
+            HashSet<int> result = new HashSet<int>();
+            for(int i=0;i<nums2.Length;i++)
+            {
+                if (list.Contains(nums2[i])&&!result.Contains(nums2[i])) result.Add(nums2[i]);
+            }
+            return result.ToArray();
+
+        }
+        public int[] Intersection1(int[] nums1, int[] nums2)
+        {
+            return nums1.Where(x => nums2.Contains(x)).Distinct().ToArray();
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_350.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_350
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var list =new  Dictionary<int, int>();
+            var result = new List<int>();
+            for(int i=0;i<nums1.Length;i++)
+            {
+                if (list.ContainsKey(nums1[i])) 
+                    list[nums1[i]]++;
+                else 
+                    list[nums1[i]] = 1;
+            }
+            for(int j=0;j<nums2.Length;j++)
+            {
+                if (list.ContainsKey(nums2[j]) && list[nums2[j]] != 0)
+                {
+                    result.Add(nums2[j]);
+                    list[nums2[j]]--;
+                }
+                    
+            }
+            return result.ToArray();
+        }
+        public int[] Intersect1(int[] nums1,int[] nums2)
+        {
+            var result1 = nums1.Where(x => nums1.Intersect(nums2).Contains(x));
+            var result2 = nums2.Where(x => nums1.Intersect(nums2).Contains(x));
+            return result1.Count() >= result2.Count() ? result2.ToArray() : result1.ToArray();
+        }
+    }
+}
