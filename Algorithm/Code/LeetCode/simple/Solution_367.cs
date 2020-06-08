bf42bcb61a546e09f016347caafc26fd798cf44a
LMing123 <z971130192@live.com>
2020-06-08 20:13:39 +0800

    add 367

---
+++ Algorithm/Code/LeetCode/simple/Solution_367.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_367
+    {
+
+        public bool IsPerfectSquare(int num)
+        {
+            if (num < 2) return true;
+            long left = 2;
+            long right = num / 2;
+            while (left <= right)
+            {
+                long x = left + (right - left) / 2;
+                long result = x * x;
+                if (result == num) return true;
+                if (result < num) left = x + 1;
+                else right = x - 1;
+            }
+            return false;
+        }
+    }
+}
