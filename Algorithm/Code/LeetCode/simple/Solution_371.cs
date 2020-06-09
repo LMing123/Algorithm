41d16c186486a808d4cff225a16b49028bb3cc41
LMing123 <z971130192@live.com>
2020-06-09 20:45:40 +0800

    add 374

---
+++ Algorithm/Code/LeetCode/simple/Solution_371.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_371
+    {
+        public int GetSum(int a, int b)
+        {
+            while (b != 0)
+            {
+                int c = (a & b) << 1;
+                a = a ^ b;
+                b = c;
+            }
+            return a;
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_374.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_374
+    {
+        public int GuessNumber(int n)
+        {
+            int min = 1;
+            int max = n;
+            int num = 0;
+            while (min <= max)
+            {
+                num = min + (max - min) / 2;
+                var result = guess(num);
+                if (result == 0) return num;
+                else if (result == 1) min = num + 1;
+                else max = num - 1;
+            }
+            return num;
+        }
+        public int guess(int num)
+        {
+            return 0;
+        }
+    }
+}
