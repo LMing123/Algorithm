78f7d9d98f042dc30d36700e4f235a6c7de84010
LMing123 <z971130192@live.com>
2020-06-10 19:58:44 +0800

    add  383

---
+++ Algorithm/Code/LeetCode/simple/Solution_383.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_383
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<char, int> list = new Dictionary<char, int>();
+            for(int i=0;i<magazine.Length;i++)
+            {
+                if(list.ContainsKey(magazine[i]))
+                {
+                    list[magazine[i]]++;
+                }
+                else
+                {
+                    list.Add(magazine[i], 1);
+                }
+            }
+
+            for(int j=0;j<ransomNote.Length;j++)
+            {
+                if(list.ContainsKey(ransomNote[j]))
+                {
+                    if (list[ransomNote[j]] <= 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        list[ransomNote[j]]--;
+                    }
+                }
+                else
+                {
+                    return false;
+                }               
+            }
+            return true;
+        }
+    }
+}
