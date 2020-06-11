4b75010ff096a16ffb82797051e3a960e95fce95
LMing123 <z971130192@live.com>
2020-06-11 19:52:33 +0800

    add 387

---
+++ Algorithm/Code/LeetCode/simple/Solution_387.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_387
+    {
+        public int FirstUniqChar(string s)
+        {
+            if (s.Length == 0) return -1;
+            int[] list = new int[26];
+            for(int index=0;index<s.Length;index++)
+            {
+                int value = s[index] - 'a';
+                list[value]++;
+            }
+            for (int index = 0; index < s.Length; index++)
+            {
+                int value = s[index] - 'a';
+                if (list[value] == 1) return index;
+            }
+            return -1;
+        }
+    }
+}
