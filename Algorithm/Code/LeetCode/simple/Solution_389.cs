ef9e6f7ea50eabf364f9991350581fe8f632b745
LMing123 <z971130192@live.com>
2020-06-15 20:50:58 +0800

    add  389 392

---
+++ Algorithm/Code/LeetCode/simple/Solution_389.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_389
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            int[] list = new int[26];
+            for(int index=0;index<s.Length;index++)
+            {
+                list[s[index] - 'a']++;
+            }
+
+            for(int index=0;index<t.Length;index++)
+            {
+                list[t[index] - 'a']--;
+                if (list[t[index] - 'a'] < 0) return t[index];
+            }
+            return '0';
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_392.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_392
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0 && t.Length != 0) return true;
+            if (s.Length != 0 && t.Length == 0) return false;
+            if (s.Length == 0 && t.Length == 0) return true;
+            int index = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (s[index] == t[i]) index++;
+                if (index >= s.Length) return true;
+            }
+            return false;
+        }
+    }
+}
