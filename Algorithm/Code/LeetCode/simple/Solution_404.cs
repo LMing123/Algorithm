d7774fc168eefe20a6aef553d82874c692c5c7d9
LMing123 <z971130192@live.com>
2020-06-22 20:19:13 +0800

    add 404

---
+++ Algorithm/Code/LeetCode/simple/Solution_404.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.XPath;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_404
+    {
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            int result = 0;
+            if (root == null) return 0;
+            if(root.left!=null)
+            {
+                if (root.left.left == null && root.left.right == null)
+                {
+                    result += root.left.val;
+                }
+            }
+            result += SumOfLeftLeaves(root.left);
+            result += SumOfLeftLeaves(root.right);
+            return result;
+
+        }
+    }
+}
