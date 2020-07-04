f07fd8cbb41e5a4abe80af3ce2358a128eef1793
LMing123 <z971130192@live.com>
2020-07-04 17:55:58 +0800

    add 441

---
+++ Algorithm/Code/LeetCode/simple/Solution_441.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_441
+    {
+        public int ArrangeCoins(int n)
+        {
+            return (int)(Math.Sqrt(2 * (double)n + 0.25) - 0.5);
+            // if(n==0) return 0;
+            //for(int i=1; ;i++)
+            //{
+            //    int result = ((1 + i) * i) / 2;
+            //    int j = i + 1;
+            //    int tem= ((1 + j) * j) / 2;
+            //    if (result <= n && tem > n) return i;
+            //}
+
+        }
+    }
+}
