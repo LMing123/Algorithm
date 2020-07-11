b604685c4ddb6e6a64d78fc65f28daa3137885d4
LMing123 <z971130192@live.com>
2020-07-11 17:22:13 +0800

    add 448

---
+++ Algorithm/Code/LeetCode/simple/Solution_448.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_448
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            List<int> list = new List<int>(nums.Length);
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (nums[Math.Abs(nums[i])-1] < 0) continue;
+                nums[Math.Abs(nums[i]) - 1] = nums[Math.Abs(nums[i]) - 1] * -1;
+            }
+
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (nums[i] >= 0) list.Add(i + 1);
+            }
+
+            return list;
+        }
+    }
+}
