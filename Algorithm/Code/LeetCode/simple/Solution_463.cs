b0dc75fb05a9c1ac6cedb39c3d879eee64540ecc
LMing123 <z971130192@live.com>
2020-07-23 20:00:25 +0800

    add 463

---
+++ Algorithm/Code/LeetCode/simple/Solution_463.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_463
+    {
+        public int IslandPerimeter(int[][] grid)
+        {
+            var result = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 0) continue;
+                    if (i - 1 < 0) result++;
+                    if (i + 1 >= grid.Length) result++;
+                    if (j - 1 < 0) result++;
+                    if (j + 1 >= grid[i].Length) result++;
+                    if (i - 1 >= 0 && grid[i - 1][j] == 0) result++;
+                    if (i + 1 < grid.Length && grid[i + 1][j] == 0) result++;
+                    if (j - 1 >= 0 && grid[i][j - 1] == 0) result++;
+                    if (j + 1 < grid[i].Length && grid[i][j + 1] == 0) result++;
+                }
+            }
+            return result;
+        }
+    }
+}
