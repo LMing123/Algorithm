dd8ce18a864f681080eeccb6fdee123d96298c41
LMing123 <z971130192@live.com>
2020-08-03 19:16:19 +0800

    add 482

---
+++ Algorithm/Code/LeetCode/simple/Solution_482.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_482
+    {
+        public static string LicenseKeyFormatting(string S, int K)
+        {
+            var sb = new StringBuilder(S.Length);
+            char stack = '-';
+            int step = 0;
+            for(int i=S.Length-1;i>=0;i--)
+            {
+                if (S[i] == '-') continue;
+                if (step >= K)
+                {
+                    step = 0;
+                    sb.Append(stack);
+                }
+               
+                sb.Append(char.ToUpper( S[i]));
+                step++;
+            }
+            return new string(sb.ToString().Reverse().ToArray());
+        }
+    }
+}
