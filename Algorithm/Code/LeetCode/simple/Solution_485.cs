182e5b5ea11844252cff67d5e2919794618890c2
LMing123 <z971130192@live.com>
2020-08-04 21:10:05 +0800

    add 485

---
+++ Algorithm/Code/LeetCode/simple/Solution_485.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_485
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int result = 0;
+            int tem = 0;
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (nums[i] == 1)
+                {
+                    tem++;
+                    result = tem > result ? tem : result;
+                }
+                else
+                {
+                    result = tem > result ? tem : result;
+                    tem = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
