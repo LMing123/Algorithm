f6d07794230920d0972d0832681057fd0601e103
LMing123 <z971130192@live.com>
2020-08-10 20:55:20 +0800

    add 492

---
+++ Algorithm/Code/LeetCode/simple/Solution_492.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    public class Solution_492
+    {
+        public static int[] ConstructRectangle(int area)
+        {
+            if (area == 0) return new int[] { 0, 0 };
+            int[] result = new int[] {  area,1 };
+            int min = area - 1; ;
+
+            for(int i=2;i< Math.Sqrt(area);i++)
+            {
+                if (area % i != 0) continue;
+                int j = area / i;
+                if(Math.Abs( j-i)<min)
+                {
+                    min = Math.Abs(j-i);
+                    result = new int[] { j,i};
+                }               
+            }
+            return result;
+        }
+    }
+}
