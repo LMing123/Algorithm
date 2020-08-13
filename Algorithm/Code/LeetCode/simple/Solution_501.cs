056fec257356f81e2ea94d31bd6dc8ee1106aa80
LMing123 <z971130192@live.com>
2020-08-13 21:48:54 +0800

    add 501 2

--- Algorithm/Code/LeetCode/simple/Solution_501.cs
+++ Algorithm/Code/LeetCode/simple/Solution_501.cs
@@ -8,28 +8,74 @@ namespace Algorithm.Code.LeetCode.simple
 {
     class Solution_501
     {
-        Dictionary<int, int> lists = new Dictionary<int, int>();
+        int maxCount = 0;
+        int currCount = 0;
+        int allCount = 0;
+        int[] list = null;
+        TreeNode prev = null;
+
         public int[] FindMode(TreeNode root)
         {
-            if (root == null) return new int[] { };
-            SearchTree(root);
-            var result = lists.GroupBy(x => x.Value).OrderByDescending(y => y.Key).FirstOrDefault().ToList().Select(x => x.Key);
-            return result.ToArray();
-
+            HelpMethod(root);
+            list = new int[allCount];
+            currCount = 0;
+            allCount = 0;
+            HelpMethod(root);
+            return list;
         }
-        public void SearchTree(TreeNode root)
+
+        void HelpMethod(TreeNode root)
         {
             if (root == null) return;
-            if(lists.ContainsKey(root.val))
+            HelpMethod(root.left);
+            if (prev!= null && prev.val == root.val)
             {
-                lists[root.val] += 1;
+                currCount++;
             }
             else
             {
-                lists.Add(root.val, 1);
+                currCount = 1;
             }
-            SearchTree(root.left);
-            SearchTree(root.right);
+            
+            if (currCount > maxCount)
+            {
+                maxCount = currCount;
+                allCount = 1;
+            }else if(currCount==maxCount)
+            {
+                if (list != null)
+                    list[allCount] = root.val;
+                allCount++;
+            }
+            
+            HelpMethod(root.right);
+            prev = root;
         }
+
+        #region first method
+        //Dictionary<int, int> lists = new Dictionary<int, int>();
+        //public int[] FindMode(TreeNode root)
+        //{
+        //    if (root == null) return new int[] { };
+        //    SearchTree(root);
+        //    var result = lists.GroupBy(x => x.Value).OrderByDescending(y => y.Key).FirstOrDefault().ToList().Select(x => x.Key);
+        //    return result.ToArray();
+
+        //}
+        //public void SearchTree(TreeNode root)
+        //{
+        //    if (root == null) return;
+        //    if(lists.ContainsKey(root.val))
+        //    {
+        //        lists[root.val] += 1;
+        //    }
+        //    else
+        //    {
+        //        lists.Add(root.val, 1);
+        //    }
+        //    SearchTree(root.left);
+        //    SearchTree(root.right);
+        //}
+        #endregion
     }
 }
