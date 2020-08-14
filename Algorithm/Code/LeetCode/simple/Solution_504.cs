fb5e88635ad8a89e19f9e72584cd4d434c2a8778
LMing123 <z971130192@live.com>
2020-08-14 19:03:50 +0800

    add 504

---
+++ Algorithm/Code/LeetCode/simple/Solution_504.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_504
+    {
+        public string ConvertToBase7(int num)
+        {
+            StringBuilder sb = new StringBuilder();
+            int quotient = Math.Abs(num);
+            if(quotient<7)
+            {
+                sb.Append(quotient);
+            }
+            else
+            {
+               
+                while (quotient >= 7)
+                {
+                    int reminder = quotient % 7;
+                    quotient = num / 7;
+                    sb.Append(reminder);
+                }
+                sb.Append(quotient);
+            }
+            if (num < 0) sb.Append('-');
+            return new string(sb.ToString().Reverse().ToArray());
+            
+        }
+    }
+}
