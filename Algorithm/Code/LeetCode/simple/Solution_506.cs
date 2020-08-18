6b063cffb3d4dc363ce1baaa6e2b774f014eb18e
LMing123 <z971130192@live.com>
2020-08-18 20:59:53 +0800

    add 507

---
+++ Algorithm/Code/LeetCode/simple/Solution_506.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_506
+    {
+        public string[] FindRelativeRanks(int[] nums)
+        {
+            Dictionary<int, int> lists = new Dictionary<int, int>(nums.Length);
+            string[] result = new string[nums.Length];
+            for(int i=0;i<nums.Length;i++)
+            {
+                lists.Add(nums[i], i);
+            }
+
+            Array.Sort<int>(nums);
+            int j = 1;
+            for(int i=nums.Length-1;i>=0;i--,j++)
+            {
+                if (j == 1)
+                    result[lists[nums[i]]] = "Gold Medal";
+                else if (j == 2)
+                    result[lists[nums[i]]] = "Silver Medal";
+                else if(j==3)
+                    result[lists[nums[i]]] = "Bronze Medal";
+                else
+                    result[lists[nums[i]]] = j.ToString();
+
+            }
+            return result;
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_507.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_507
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            int sum = 1;
+            for(int i=2;i*i<=num;i++)
+            {
+                if(num%i==0)
+                {
+                    sum += i;
+                        sum += num / i;
+                }
+            }
+            return sum == num;
+        }
+    }
+}
