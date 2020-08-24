606003ee0e82e25f05af447293e70e3f7648a903
LMing123 <z971130192@live.com>
2020-08-24 21:18:07 +0800

    add 530

---
+++ Algorithm/Code/LeetCode/simple/Solution_521.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_521
+    {
+        public int FindLUSlength(string a, string b)
+        {
+            if (a.Length != b.Length) return Math.Max(a.Length, b.Length);
+            if (a == b) return -1;
+            return a.Length;
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_530.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_530
+    {
+        public int GetMinimumDifference(TreeNode root)
+        {
+            List<int> list = new List<int>();
+            TravelTree(root, ref list);
+            int result = int.MaxValue;
+            for(int i=1;i<list.Count; i++)
+            {
+                if ((list[i] - list[i - 1]) < result) 
+                    result = list[i] - list[i - 1];
+            }
+            return result;
+        }
+
+       void TravelTree(TreeNode root,ref List<int> lists)
+        {
+            if (root == null) return;
+            TravelTree(root.left,ref lists);
+            lists.Add(root.val);
+            TravelTree(root.right, ref lists);
+        }
+    }
+}
