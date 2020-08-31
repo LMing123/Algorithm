2676ca1a3988fe59c04ef5786e9bf87dd4c91df3
LMing123 <z971130192@live.com>
2020-08-31 21:47:17 +0800

    add 543

---
+++ Algorithm/Code/LeetCode/simple/Solution_543.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_543
+    {
+        int max = 0;
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+             Helper(root);
+            return max;
+
+        }
+
+        public int Helper(TreeNode root)
+        {
+            if (root== null) return 0;
+
+            int left = Helper(root.left);
+            int right = Helper(root.right);
+            max = Math.Max(left + right, max);
+            return Math.Max(left, right)+1;
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -14,12 +14,15 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            var ss = new int?[] { 1, 2, 3, 4, 5, 6 };
+           // var ss = new int?[] { 4, -7, -3, null, null, -9, -3, 9, -7, -4, null, 6, null, -6, -6, null, null, 0, 6, 5, null, 9, null, null, -1, -4, null, null, null, -2 };
+            var jj = Common.BulidTree(ref ss);
 
-            var solution = new Solution_541();
-             var s=solution.ReverseStr("abcdefg",2);
+            var solution = new Solution_543();
+            var s = solution.DiameterOfBinaryTree(jj);
         }
 
-   
+
     }
 
     public class Shit
