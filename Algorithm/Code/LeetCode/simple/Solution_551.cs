d026cfd5577dac1016ece0fdf4eb8a1aa66e2238
LMing123 <z971130192@live.com>
2020-09-02 20:57:46 +0800

    add 551

---
+++ Algorithm/Code/LeetCode/simple/Solution_551.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_551
+    {
+        public bool CheckRecord(string s)
+        {
+            int ANum = 0;
+            int LNum = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case 'A':
+                        {
+                            LNum = 0;
+                            ANum++;
+                            if (ANum > 1) return false;
+                            break;
+                        }
+                    case 'L':
+                        {
+                            LNum++;                            
+                            break;
+                        }
+                    default:
+                        LNum = 0;break;
+                    
+                }
+                if (LNum > 2) return false;
+            }
+            return true;
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -18,8 +18,8 @@ namespace Algorithm
            // var ss = new int?[] { 4, -7, -3, null, null, -9, -3, 9, -7, -4, null, 6, null, -6, -6, null, null, 0, 6, 5, null, 9, null, null, -1, -4, null, null, null, -2 };
             var jj = Common.BulidTree(ref ss);
 
-            var solution = new Solution_543();
-            var s = solution.DiameterOfBinaryTree(jj);
+            var solution = new Solution_551();
+            var s = solution.CheckRecord("PPALLL");
         }
 
 
