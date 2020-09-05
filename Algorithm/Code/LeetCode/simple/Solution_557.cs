afab426349f96b9aadd2ddd77ef276587c984585
LMing123 <z971130192@live.com>
2020-09-05 21:21:25 +0800

    add 557

---
+++ Algorithm/Code/LeetCode/simple/Solution_557.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_557
+    {
+        public string ReverseWords(string s)
+        {
+            if (s.Length == 0 || string.IsNullOrEmpty(s)) return s;
+            var content = s.ToCharArray();
+
+
+            int begin = NextNoSpace(ref content, 0);
+            int index = begin + 1;
+            while (index<content.Length)
+            {     
+                while ( index < content.Length&&content[index] != ' ')
+                {
+                    index++;
+                }
+                ExchangeWord(ref content, begin, index - 1);
+                begin= NextNoSpace(ref content, index);
+                index++;
+            }
+            return new string(content);
+        }
+
+        int NextNoSpace(ref char[] content,int index)
+        {
+            for(int i=index;i<content.Length;i++)
+            {
+                if (content[i] != ' ') return i;
+            }
+            return content.Length;
+        }
+        void ExchangeWord(ref char[] content,int begin,int end)
+        {
+            while(begin<end)
+            {
+                var tem = content[begin];
+                content[begin] = content[end];
+                content[end] = tem;
+                begin++;
+                end--;
+            }
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -18,8 +18,8 @@ namespace Algorithm
            // var ss = new int?[] { 4, -7, -3, null, null, -9, -3, 9, -7, -4, null, 6, null, -6, -6, null, null, 0, 6, 5, null, 9, null, null, -1, -4, null, null, null, -2 };
             var jj = Common.BulidTree(ref ss);
 
-            var solution = new Solution_551();
-            var s = solution.CheckRecord("PPALLL");
+            var solution = new Solution_557();
+            var s = solution.ReverseWords("Let's take LeetCode contest");
         }
 
 
