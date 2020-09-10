d23e4c33ffe3bfa9c4570ad6240c4ac30fbd2035
LMing123 <z971130192@live.com>
2020-09-10 19:11:15 +0800

    add 563

---
+++ Algorithm/Code/LeetCode/simple/Solution_563.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_563
+    {
+        int result = 0;
+        public int FindTilt(TreeNode root)
+        {
+            Helper(root);
+            return result;
+        }
+
+        public int Helper(TreeNode root)
+        {
+            if (root == null) return 0;
+            int left = Helper(root.left);
+            int right = Helper(root.right);
+            result += Math.Abs(left - right);
+            return root.val+left+right;
+        }
+    }
+}
