8bbeee93053fe162cf5135273c0af0e560325b0e
LMing123 <z971130192@live.com>
2020-09-12 15:38:26 +0800

    add 572

---
+++ Algorithm/Code/LeetCode/simple/Solution_572.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_572
+    {
+        List<TreeNode> list = new List<TreeNode>();
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            FindRoot(s, t);
+            bool result = false;
+            for (int i = 0; i < list.Count; i++)
+            {
+                result = result || Helper(list[i], t);
+            }
+            return result;
+        }
+
+
+        public void FindRoot(TreeNode s, TreeNode t)
+        {
+            if (s == null) return;
+            if (s.val == t.val) list.Add(s);
+            FindRoot(s.left, t);
+            FindRoot(s.right, t);
+        }
+
+
+        public bool Helper(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null) return true;
+            if (s == null && t != null) return false;
+            if (s != null && t == null) return false;
+            if (s.val != t.val) return false;
+
+            return Helper(s.left, t.left) && Helper(s.right, t.right);
+        }
+    }
+}
