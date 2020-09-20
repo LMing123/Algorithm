cf9d8805b9869214d36baca8cb959dd7b49ae59d
LMing123 <z971130192@live.com>
2020-09-20 19:58:11 +0800

    add 594

---
+++ Algorithm/Code/LeetCode/simple/Solution_594.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_594
+    {
+        public int FindLHS(int[] nums)
+        {
+            var list = new Dictionary<int, int>();
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (list.ContainsKey(nums[i])) list[nums[i]]++;
+                else list.Add(nums[i], 1);
+            }
+
+            int max = 0;
+            foreach (var item in list)
+            {
+                if (list.ContainsKey(item.Key + 1)) max = Math.Max(max, list[item.Key + 1] + item.Value);
+                else if(list.ContainsKey(item.Key - 1)) max = Math.Max(max, list[item.Key - 1] + item.Value);
+            }
+            return max;
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -7,6 +7,8 @@ using Algorithm.Code.LeetCode.simple;
 using Algorithm.Code.StringFind;
 using Algorithm.Code.Other;
 using System.Threading;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Algorithm
 {
@@ -14,18 +16,8 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            int[][] nums = new int[][]
-            {
-                new int[]{ 1,2,3},
-                new int[]{4,5,6},
-                new int[]{4,5,6},
-                new int[]{4,5,6}
-            };
-            var i = nums.Length;
-            var i2 = nums.SelectMany(x => x);
-            nums.Any
-            var solution = new Solution_557();
-            var s = solution.ReverseWords("Let's take LeetCode contest");
+
+            int[] list = new int[] { 1, 3, 2, 2, 5, 2, 3, 7 };
         }
 
 
