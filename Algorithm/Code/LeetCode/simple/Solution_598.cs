852aa9a0003acfebf4d572cdd4ce96166af280ff
LMing123 <z971130192@live.com>
2020-09-21 19:14:56 +0800

    add 598

---
+++ Algorithm/Code/LeetCode/simple/Solution_598.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_598
+    {
+        public int MaxCount(int m, int n, int[][] ops)
+        {
+            if (ops.Length == 0) return m * n;
+            int firstDimension = int.MaxValue;
+            int secondDimension = int.MaxValue;
+            for(int i=0;i<ops.Length;i++)
+            {
+                firstDimension= Math.Min(ops[i][0], firstDimension);
+                secondDimension = Math.Min(ops[i][1], secondDimension);
+            }
+
+            return firstDimension * secondDimension;
+        }
+    }
+}
