e1ecf96eaffa3d87dbb46cf63098ca44fe62a98a
LMing123 <z971130192@live.com>
2020-09-24 17:36:55 +0800

    add 605

---
+++ Algorithm/Code/LeetCode/simple/Solution_599.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_599
+    {
+        Dictionary<string, int> list = new Dictionary<string, int>();
+
+        public string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            var sortList = list1.Length > list2.Length ? list2 : list1;
+            var sortLength = Math.Min(list1.Length , list2.Length);
+            for (int i = 0; i < sortLength;i++)
+            {
+                list.Add(sortList[i], i);
+            }
+
+         
+            if (list1.Length==sortLength)
+            {
+                return Helper(list2);
+            }
+            else
+            {
+                return Helper(list1);
+
+            }
+        }
+
+
+        public string[] Helper(string[] content)
+        {
+            int min = 2020;
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (!list.ContainsKey(content[i])) continue;
+
+                if ( i + list[content[i]] < min)
+                {
+                    result.Clear();
+                    result.Add(content[i]);
+                    min = i + list[content[i]];
+                }
+                else if ( i + list[content[i]] == min)
+                {
+                    result.Add(content[i]);
+                }
+             }
+            return result.ToArray();
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_605.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_605
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            bool canPlan = true;
+            int sum = 0;
+            for(int i=0;i<flowerbed.Length-1;i++)
+            {
+                if (flowerbed[i] == 1) canPlan = false;
+                else if (canPlan && flowerbed[i] == 0 && flowerbed[i+1] == 0)
+                {
+                    canPlan = false;
+                    sum++;
+                }
+                else if (flowerbed[i] == 0) canPlan = true;
+            }
+            if (flowerbed[flowerbed.Length - 1] == 0 && canPlan) sum++;
+            return n <= sum ? true : false;
+        }
+    }
+}
