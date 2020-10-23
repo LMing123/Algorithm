24aa1444444e90179fa85cba7cf66287f6cbfaa0
LMing123 <z971130192@live.com>
2020-10-23 18:11:57 +0800

    add 645

---
+++ Algorithm/Code/LeetCode/simple/Solution_645.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_645
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+          //  HashSet<int> vs = new HashSet<int>();
+            int[] tem = new int[nums.Length + 1];
+            int tag = 0;
+            int miss = 0;
+            for(int i=0;i<nums.Length;i++)
+            {
+               if(tem[nums[i]] == nums[i])
+                {
+                    tag = nums[i];
+                }
+                tem[nums[i]] = nums[i];
+            }
+
+            for(int i=0;i<tem.Length;i++)
+            {
+                if (tem[i] == 0) miss = i;
+            }
+            return new int[] { tag, miss };
+        }
+    }
+}
