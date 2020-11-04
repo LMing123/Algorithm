1a8f05e6c4852685cef326565c33ca3620e8ae49
LMing123 <z971130192@live.com>
2020-11-04 18:54:50 +0800

    add 669

---
+++ Algorithm/Code/LeetCode/FUCK/Solution_665.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.FUCK
+{
+    public class Solution_665
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            int total = 0;
+
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                if (nums[i] <= nums[i + 1]) continue;
+
+                total++;
+                if (total > 1) return false;
+                if (i - 1 < 0) nums[i] = nums[i + 1];
+                else
+                {
+                    if (i + 2 < nums.Length && nums[i] > nums[i + 2])
+                    {
+                        nums[i] = nums[i - 1];
+                        i = i - 1;
+                    }
+                    else nums[i + 1] = nums[i];
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Algorithm/Code/LeetCode/simple/Solution_669.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_669
+    {
+        public TreeNode TrimBST(TreeNode root, int low, int high)
+        {
+            if (root == null) return null;
+            var left = TrimBST(root.left,low,high);
+            var right = TrimBST(root.right, low, high);
+
+            root.left = left;
+            root.right = right;
+
+
+            if (root.val > high) return left;
+            else if (root.val < low) return right;
+            else return root;
+
+        }
+    }
+}
