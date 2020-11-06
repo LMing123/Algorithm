c2000266f0651c1b9fe511d13152cfabb9643f9e
LMing123 <z971130192@live.com>
2020-11-06 19:53:09 +0800

    add 671

---
+++ Algorithm/Code/LeetCode/simple/Solution_671.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_671
+    {
+        int first = -1;
+        int second = -1;
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            if (root.left == null) return -1;
+            first = root.val;
+            return second;
+        }
+
+        void Helper(TreeNode root)
+        {
+            if (root== null) return ;
+            if(root.val>first)
+            {
+                if (second == -1) second = root.val;
+                else if (root.val < second) second = root.val;
+            }
+
+            Helper(root.left);
+            Helper(root.right);
+        }
+    }
+}
