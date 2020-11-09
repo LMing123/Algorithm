e8d17cdecf689b903b0249b0b509fe6ac1fbd09b
LMing123 <z971130192@live.com>
2020-11-09 18:47:43 +0800

    add 674

---
+++ Algorithm/Code/LeetCode/simple/Solution_674.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_674
+    {
+        public int FindLengthOfLCIS(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int length = 0;
+
+            int tem = 1;
+            for(int i=1;i<nums.Length;i++)
+            {
+                if (nums[i] - nums[i - 1] > 0) tem++;
+                else tem = 1;
+                if (tem > length) length = tem;
+            }
+            return length;
+        }
+    }
+}
