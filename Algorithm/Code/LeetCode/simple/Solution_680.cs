0513ed08ae0f9ed2cf4834e5c897330c2fc6b84f
LMing123 <z971130192@live.com>
2020-11-11 18:40:59 +0800

    add 680

---
+++ Algorithm/Code/LeetCode/simple/Solution_680.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_680
+    {
+        public bool ValidPalindrome(string s)
+        {
+            if (s.Length == 0) return false;
+            return Helper(ref s, 0, s.Length - 1, true);
+        }
+
+
+        public static bool Helper(ref string s ,int start, int end,bool canRemove)
+        {
+            int begin = start;
+            while (begin < end)
+            {
+                if (s[begin] == s[end])
+                {
+                    begin++;
+                    end--;
+                }
+                else if (canRemove)
+                {
+                    return Helper(ref s, begin + 1, end, false)||Helper(ref s,begin,end-1,false);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -20,8 +20,8 @@ namespace Algorithm
             int[][] list = new int[5][] { new[] { 2, 3, 4 }, new[] { 5, 6, 7 }, new[] { 8, 9, 10 }, new[] { 11, 12, 13 }, new[] { 14, 15, 16 } };
             //int[] list1 = new int[] { 2, 2, 3, 2, 4};
             int[] list1 = new int[] { 3,4,2,3};
-            var s = new Solution_665();
-            var fuck = s.CheckPossibility(list1);
+            var s = new Solution_680();
+            var fuck = s.ValidPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga");
         }
 
 
