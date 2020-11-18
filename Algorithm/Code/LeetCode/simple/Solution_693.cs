699af29d9209cb5b48c49703436b7abc37f5a02b
LMing123 <z971130192@live.com>
2020-11-18 20:23:27 +0800

    add 693

---
+++ Algorithm/Code/LeetCode/simple/Solution_693.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_693
+    {
+        public bool HasAlternatingBits(int n)
+        {
+           
+            int i = n & 1;
+            n = n >> 1;
+            while(n>0)
+            {
+                i = ~i&1;
+                if ((n & 1) != i) return false;
+                n = n >> 1;
+            }
+            return true;
+        }
+    }
+}
