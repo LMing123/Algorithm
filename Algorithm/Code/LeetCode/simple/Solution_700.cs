650172a55c3e2adc61b70338746ab9d865febec0
LMing123 <z971130192@live.com>
2020-11-27 19:05:08 +0800

    add 700

---
+++ Algorithm/Code/LeetCode/simple/Solution_700.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    class Solution_700
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null) return null;
+            if (root.val == val) return root;
+            var result = SearchBST(root.left, val);
+            if (result != null) return result;
+            return SearchBST(root.right, val);
+
+        }
+    }
+}
