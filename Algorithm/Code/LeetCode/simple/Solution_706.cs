ebae408b8be58ddd510903a068cb06772755f792
LMing123 <z971130192@live.com>
2021-02-24 19:53:59 +0800

    add 706

---
+++ Algorithm/Code/LeetCode/simple/Solution_706.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Code.LeetCode.simple
+{
+    /// <summary>
+    /// Solution_706
+    /// </summary>
+    class MyHashMap
+    {
+        private int[] lists;
+        public MyHashMap()
+        {
+            lists = new int[1000001];
+            var span=new Span<int>(lists);
+            span.Fill(-1);
+        }
+
+        /** value will always be non-negative. */
+        public void Put(int key, int value)
+        {
+            lists[key] = value;
+        }
+
+        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+        public int Get(int key)
+        {
+            return lists[key]==-1?-1:lists[key];
+        }
+
+        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+        public void Remove(int key)
+        {
+            lists[key] = -1;
+        }
+    }
+}
