adc2c3ad5c28a070ce6e0f7954f24df8864c4598
LMing123 <z971130192@live.com>
2021-06-20 21:51:27 +0800

    ADD 39

---
+++ Algorithm/Code/LeetCode/medium/Solution_39.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Code.LeetCode.medium
+{
+    class Solution_39
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            Array.Sort(candidates);
+
+            return Temp(ref candidates, target);
+
+           
+
+        }
+
+        private List<IList<int>> Temp(ref int[] sets, int target)
+        {
+            var min = sets[0];
+            if (target < min) return null;
+            List<IList<int>> result = new List<IList<int>>();           
+            for (int i = 0; i < sets.Length; i++)
+            {
+                IList<int> tem = new List<int>();
+                if (target - sets[i] == 0)
+                {
+                    tem.Add(sets[i]);
+                    result.Add(tem);
+                    continue;
+                }
+                if (target > sets[i])
+                {
+                    var tem_sets = sets[i..];
+                    var values = Temp(ref tem_sets, target - sets[i]);
+                    if(values==null) continue;
+                    foreach (var item in values)
+                    {
+                        if (item.Count > 0)
+                        {
+                            item.Add(sets[i]);
+                            result.Add(item);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -17,27 +17,33 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            var lists = new int[] { 2, 3, 1, 4, 5, 7, 6, 9, 8, 4, 22, 66, 44 };
-            QuickSort.Sort2(ref lists, 0, lists.Length - 1);
 
-            int[][] list = new int[5][] { new[] { 2, 3, 4 }, new[] { 5, 6, 7 }, new[] { 8, 9, 10 }, new[] { 11, 12, 13 }, new[] { 14, 15, 16 } };
-            //int[] list1 = new int[] { 2, 2, 3, 2, 4};
-            int[] list1 = new int[] { 3, 4, 2, 3 };
-            var s = new Solution_36();
-            char[][] shit = new char[9][] {new char[]{'5','3','.','.','7','.','.','.','.'},
-                                           new char[]{'6','.','.','1','9','5','.','.','.'},
-                                           new char[]{'.','9','8','.','.','.','.','6','.'},
-                                           new char[]{'8','.','.','.','6','.','.','.','3'},
-                                           new char[]{'4','.','.','8','.','3','.','.','1'},
-                                           new char[]{'7','.','.','.','2','.','.','.','6'},
-                                           new char[]{'.','6','.','.','.','.','2','8','.'},
-                                           new char[]{'.','.','.','4','1','9','.','.','5'},
-                                           new char[]{'.','.','.','.','8','.','.','7','9'}};
-
-            char[][] shit1 = { new char[9] { '.', '.', '5', '.', '.', '.', '.', '.', '6' }, new char[9] { '.', '.', '.', '.', '1', '4', '.', '.', '.' }, new char[9] { '.', '.', '.', '.', '.', '.', '.', '.', '.' }, new char[9] { '.', '.', '.', '.', '.', '9', '2', '.', '.' }, new char[9] { '5', '.', '.', '.', '.', '2', '.', '.', '.' }, new char[9] { '.', '.', '.', '.', '.', '.', '.', '3', '.' }, new char[9] { '.', '.', '.', '5', '4', '.', '.', '.', '.' }, new char[9] { '3', '.', '.', '.', '.', '.', '4', '2', '.' }, new char[9] { '.', '.', '.', '2', '7', '.', '6', '.', '.' } };
+            var g = new Solution_39();
+           // var result=g.CombinationSum(new int[] { 2, 3, 6, 7 },7);
+            var result=g.CombinationSum(new int[] { 2, 3,5 },8);
 
 
-            var fuck = s.IsValidSudoku(shit1);
+            //var lists = new int[] { 2, 3, 1, 4, 5, 7, 6, 9, 8, 4, 22, 66, 44 };
+            //QuickSort.Sort2(ref lists, 0, lists.Length - 1);
+
+            //int[][] list = new int[5][] { new[] { 2, 3, 4 }, new[] { 5, 6, 7 }, new[] { 8, 9, 10 }, new[] { 11, 12, 13 }, new[] { 14, 15, 16 } };
+            ////int[] list1 = new int[] { 2, 2, 3, 2, 4};
+            //int[] list1 = new int[] { 3, 4, 2, 3 };
+            //var s = new Solution_36();
+            //char[][] shit = new char[9][] {new char[]{'5','3','.','.','7','.','.','.','.'},
+            //                               new char[]{'6','.','.','1','9','5','.','.','.'},
+            //                               new char[]{'.','9','8','.','.','.','.','6','.'},
+            //                               new char[]{'8','.','.','.','6','.','.','.','3'},
+            //                               new char[]{'4','.','.','8','.','3','.','.','1'},
+            //                               new char[]{'7','.','.','.','2','.','.','.','6'},
+            //                               new char[]{'.','6','.','.','.','.','2','8','.'},
+            //                               new char[]{'.','.','.','4','1','9','.','.','5'},
+            //                               new char[]{'.','.','.','.','8','.','.','7','9'}};
+
+            //char[][] shit1 = { new char[9] { '.', '.', '5', '.', '.', '.', '.', '.', '6' }, new char[9] { '.', '.', '.', '.', '1', '4', '.', '.', '.' }, new char[9] { '.', '.', '.', '.', '.', '.', '.', '.', '.' }, new char[9] { '.', '.', '.', '.', '.', '9', '2', '.', '.' }, new char[9] { '5', '.', '.', '.', '.', '2', '.', '.', '.' }, new char[9] { '.', '.', '.', '.', '.', '.', '.', '3', '.' }, new char[9] { '.', '.', '.', '5', '4', '.', '.', '.', '.' }, new char[9] { '3', '.', '.', '.', '.', '.', '4', '2', '.' }, new char[9] { '.', '.', '.', '2', '7', '.', '6', '.', '.' } };
+
+            //var fuck = s.IsValidSudoku(shit1);
+
             // var fuck = s.ValidPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga");
             //var lists3 = new int[][] { new int[] { 1, 1, 1 }, new int[] { 1, 1, 0 }, new int[] { 1, 0, 1 } };
             // var lists2 = new int[][] { new int[] { 0, 0, 0 }, new int[] { 0, 0, 0 }};
